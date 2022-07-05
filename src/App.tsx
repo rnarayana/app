1ef8eced068698d1640e381000c9b98a8d6a60e3
Narayana Rengaswamy <narayana.rengaswamy@optym.com>
2022-07-05 14:03:10 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { datadogLogs } from "@datadog/browser-logs";
+import { datadogRum } from "@datadog/browser-rum";
+import path from "path";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.initializeAppConfiguration();
+  }
+
+  initializeAppConfiguration() {
+    const fetchConfigPromise = this.fetchConfig();
+    Promise.resolve(fetchConfigPromise).then((configJSON) => {
+      console.log(configJSON);
+      if (configJSON) {
+        datadogRum.init({
+          applicationId: configJSON.datadog.applicationId,
+          clientToken: configJSON.datadog.clientToken,
+          site: configJSON.datadog.site,
+          service: configJSON.datadog.service,
+          env: configJSON.appEnv,
+          version: configJSON.appVersion,
+          sampleRate: 100,
+          premiumSampleRate: 0,
+          trackInteractions: true
+        });
+        datadogLogs.init({
+          clientToken: configJSON.datadog.clientToken,
+          site: configJSON.datadog.site,
+          service: configJSON.datadog.service,
+          env: configJSON.appEnv,
+          version: configJSON.appVersion,
+          forwardErrorsToLogs: true,
+          sampleRate: 100,
+        });
+      }
+    });
+  }
+
+  fetchConfig() {
+    return fetch(path.resolve(__dirname, "./externalConfig/config.json")).then((response) =>response.json());
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
